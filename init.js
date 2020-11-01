f9c89b3c061dcdc677fa6034307d8f88fbb34b11
vdolce <48453232+vdolce@users.noreply.github.com>
2020-11-01 19:28:09 +0100

    develop/ -s command to create store.js into <path>

--- init.js
+++ init.js
@@ -18,32 +18,51 @@ let cleanString = (arg)=>{
 if(argvs.length == 0)
     console.error('\x1b[31m%s\x1b[0m', 'Missing the store path')
 
-if(argvs.length > 1)
-    console.error('\x1b[31m%s\x1b[0m', 'Only one argument required')   
+if(argvs.length > 2)
+    console.error('\x1b[31m%s\x1b[0m', 'Too many arguments')   
 
 if(argvs.length == 1){
     var storePath = cleanString(argvs[0]) 
     fs.writeFile('./templates/storePath.txt', storePath , (error, file) =>{
         if (error) 
             throw err;
-        else{
+        else
             console.log( '\x1b[32m%s\x1b[0m', "You've settled " + storePath + " as Store path");
-            
-            // create the store folder and the store.js file
-            fs.mkdir(storePath, {recursive:true}, (error)=>{
-                if (error) 
-                    console.log(error)
-                else{
-                    fs.writeFile(storePath + 'store.js', '', (error, file) =>{
-                        if (error) 
-                            console.log(error)
-                        console.log( '\x1b[32m%s\x1b[0m', 'store.js has been created' );
-                    });
-                }
-            })
-        }
-        
     });
+
+
+}
+if(argvs.length == 2){
+
+    // create also store.js
+    if( argvs[1] == "-s" || argvs[1] == "--store"){
+        var storePath = cleanString(argvs[0]) 
+        fs.writeFile('./templates/storePath.txt', storePath , (error, file) =>{
+            if (error) 
+                throw err;
+            else{
+                console.log( '\x1b[32m%s\x1b[0m', "You've settled " + storePath + " as Store path");
+                
+                // create the store folder and the store.js file
+                fs.mkdir(storePath, {recursive:true}, (error)=>{
+                    if (error) 
+                        console.log(error)
+                    else{
+                        fs.copyFile('./templates/store.js', storePath + 'store.js', (err) => {
+                            if (err) throw err;
+                            console.log('\x1b[32m%s\x1b[0m', 'store.js has been created');
+                        });
+                    }
+                })
+            }
+            
+        });
+    }
+    else{
+        console.error('\x1b[31m%s\x1b[0m', 'Command not found')   
+        console.error('gen-vuex-init <path> -s \t initialize path store & create store.js into <path> location')   
+    }
+    
 
 
 }
