190d5a9e59c8729cf02870f01171a1ab7341311c
vdolce <48453232+vdolce@users.noreply.github.com>
2020-11-01 11:17:15 +0100

    develop/colors terminal  & template index

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const process = require('process')
 const [,, ...argvs] = process.argv
 
 if(argvs.length==0)
-    console.error('Missing the view/component name')
+    console.error('\x1b[31m%s\x1b[0m', 'Missing the view/component name')
 
 var storePath = './src/store/modules/'
 
@@ -14,26 +14,21 @@ argvs.forEach((element)=>{
     // create folders recursively
     fs.mkdir(modulePath, {recursive:true}, (error)=>{
         if (error) {
-            console.log(error)
+            console.log('\x1b[31m%s\x1b[0m', error)
           } else {
-            console.log("Directory " + modulePath + " successfully created.")
+            console.log('\x1b[32m%s\x1b[0m', "Directory " + modulePath + " successfully created.")
           }
 
           // create index.js w/ content from a txt file   
         fs.writeFile(modulePath + 'index.js','', (err, file) =>{
             if (err) throw err;
-            console.log('index.js has been created in ' + modulePath);
+            console.log('\x1b[32m%s\x1b[0m', 'index.js has been created in ' + modulePath);
         });
 
         // open destination file (index.js) for appending
         var w = fs.createWriteStream(modulePath + 'index.js', {flags: 'a'});
         // open source file (indexStoreContent.txt) for reading
-        var r = fs.createReadStream('indexStoreContent.txt');
-
-        w.on('close', function() {
-            console.log("done writing");
-        });
-        
+        var r = fs.createReadStream('indexStoreTemplate.txt');      
         r.pipe(w);
 
         // create state, actions, mutations, getters JS files
@@ -42,7 +37,7 @@ argvs.forEach((element)=>{
             fs.writeFile(modulePath + element +'.js', 'export default{\n\n}', (error, file) =>{
                 if (error) 
                     throw err;
-                console.log( element + ' has been created in ' + modulePath );
+                console.log( '\x1b[32m%s\x1b[0m', element + '.js has been created in ' + modulePath );
             });
         })
 
